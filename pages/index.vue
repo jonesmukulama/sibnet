31e059d12e7ca529019481e6deea97f6e1a634c6
Jones <jonesmukulama@gmail.com>
2023-03-27 01:45:41 +0200

    homepage update

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,31 @@
 <template>
  <div class="min-h-screen">
-  <div class="h-[540px] bg-slate-300 overflow-hidden flex"></div>
+  <div class="h-[660px] bg-slate-700 overflow-hidden flex"></div>
+  <div class="flex justify-between items-center h-56">
+   <div class="flex justify-between w-10/12 mx-auto">
+    <div class="w-1/2">
+     <blockquote class="font-semibold italic text-orange-400 text-3xl">"Lorem ipsum dolor sit amet consectetur adipisicing elit"</blockquote>
+    </div>
+    <div class="w-1/2">
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat recusandae, quo magni ullam praesentium, consectetur obcaecati atque ipsum officia illo vero, ipsam corrupti fugit voluptate rerum dignissimos reprehenderit quidem consequatur.</p>
+    </div>
+   </div>
+  </div>
+  <div class="h-96">
+   <h1 class="font-black text-2xl text-center">products &amp; services</h1>
+  </div>
+  <div class="h-96 bg-slate-100">
+   <h1 class="font-black text-2xl text-center">Latest Posts</h1>
+  </div>
+  <div class="h-52">
+   <h1 class="font-black text-2xl text-center">clients</h1>
+  </div>
+  <div class="flex flex-col justify-center items-center h-[600px]">
+   <h1 class="font-black text-2xl text-center">contact us</h1>
+   <input type="text" class="border-2 border-orange-400 w-2/5 h-12 mb-5" placeholder="Your Name" />
+   <input type="text" class="border-2 border-orange-400 w-2/5 h-12 mb-5" placeholder="Your email e.g. johnsmith@example.com" />
+   <textarea rows="10" class="border-2 border-orange-400 w-2/5 mb-5">Your Message</textarea>
+   <button class="bg-orange-400 w-36 h-12">Send Message</button>
+  </div>
  </div>
 </template>
